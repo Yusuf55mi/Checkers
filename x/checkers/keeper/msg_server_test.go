ee4dc96e9acdf6ba9a5b1d9882c180aaf8271b50
Developer Experience team at Ignite <hello@ignite.com>
2023-08-29 11:39:05 +0300

    Initialized with Ignite CLI

---
+++ x/checkers/keeper/msg_server_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers/keeper"
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	ms, ctx := setupMsgServer(t)
+	require.NotNil(t, ms)
+	require.NotNil(t, ctx)
+}
