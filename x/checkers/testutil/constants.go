10f5ea06570124ada0017217415d6691f3934550
Yusuf55mi <yusuf55@gmail.com>
2023-08-30 13:18:41 +0300

    feat(Checkers): :pushpin: first message created with ignite scaffold message and unit tests added

--- x/checkers/testutil/constants.go
+++ x/checkers/testutil/constants.go
@@ -3,4 +3,5 @@ package testutil
 const (
 	Alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
 	Bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
+	Carol = "cosmos1xv9tklw7d82sezh9haa573wufgy59vmwe6xxe5"
 )
