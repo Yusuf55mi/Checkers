691f632c6f0f1063c8577a87b5719d49548233c8
Yusuf55mi <yusuf55@gmail.com>
2023-09-07 13:58:42 +0300

    feat(Checkers): :rocket: The project finished

--- x/checkers/keeper/end_block_server_game.go
+++ x/checkers/keeper/end_block_server_game.go
@@ -49,12 +49,17 @@ func (k Keeper) ForfeitExpiredGames(goCtx context.Context) {
 			lastBoard := storedGame.Board
 			if storedGame.MoveCount <= 1 {
 				// No point in keeping a game that was never really played
+				if storedGame.MoveCount == 1 {
+					k.MustRefundWager(ctx, &storedGame)
+				}
 				k.RemoveStoredGame(ctx, gameIndex)
+
 			} else {
 				storedGame.Winner, found = opponents[storedGame.Turn]
 				if !found {
 					panic(fmt.Sprintf(types.ErrCannotFindWinnerByColor.Error(), storedGame.Turn))
 				}
+				k.MustPayWinnings(ctx, &storedGame)
 				storedGame.Board = ""
 				k.SetStoredGame(ctx, storedGame)
 			}
--- x/checkers/keeper/wager_handler_test.go
+++ x/checkers/keeper/wager_handler_test.go
@@ -56,6 +56,37 @@ func TestWagerHandlerCollectFailedNoMove(t *testing.T) {
 	require.EqualError(t, err, "black cannot pay the wager: oops")
 }
 
+func TestWagerHandlerCollectWrongNoRed(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "red address is invalid: : empty address string is not allowed", r)
+	}()
+	keeper.CollectWager(ctx, &types.StoredGame{
+		MoveCount: 1,
+	})
+}
+
+func TestWagerHandlerCollectFailedOneMove(t *testing.T) {
+	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	red, _ := sdk.AccAddressFromBech32(bob)
+	escrow.EXPECT().
+		SendCoinsFromAccountToModule(ctx, red, types.ModuleName, gomock.Any()).
+		Return(errors.New("oops"))
+	err := keeper.CollectWager(ctx, &types.StoredGame{
+		Red:       bob,
+		MoveCount: 1,
+		Wager:     45,
+	})
+	require.NotNil(t, err)
+	require.EqualError(t, err, "red cannot pay the wager: oops")
+}
+
 func TestWagerHandlerCollectNoMove(t *testing.T) {
 	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
 	ctx := sdk.UnwrapSDKContext(context)
@@ -67,6 +98,66 @@ func TestWagerHandlerCollectNoMove(t *testing.T) {
 		Wager:     45,
 	})
 	require.Nil(t, err)
+}
+
+func TestWagerHandlerCollectOneMove(t *testing.T) {
+	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	escrow.ExpectPay(context, bob, 45)
+	err := keeper.CollectWager(ctx, &types.StoredGame{
+		Red:       bob,
+		MoveCount: 1,
+		Wager:     45,
+	})
+	require.Nil(t, err)
+}
+
+func TestWagerHandlerPayWrongNoWinnerAddress(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "black address is invalid: : empty address string is not allowed", r)
+	}()
+	keeper.MustPayWinnings(ctx, &types.StoredGame{
+		Winner: "b",
+	})
+}
+
+func TestWagerHandlerPayWrongWinnerNotFound(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "cannot find winner by color: *", r)
+	}()
+	keeper.MustPayWinnings(ctx, &types.StoredGame{
+		Black:  alice,
+		Red:    bob,
+		Winner: "*",
+	})
+}
+
+func TestWagerHandlerPayWrongNotPayTime(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "there is nothing to pay, should not have been called", r)
+	}()
+	keeper.MustPayWinnings(ctx, &types.StoredGame{
+		Black:     alice,
+		Red:       bob,
+		Winner:    "b",
+		MoveCount: 0,
+	})
 }
 
 func TestWagerHandlerPayWrongEscrowFailed(t *testing.T) {
@@ -92,6 +183,20 @@ func TestWagerHandlerPayWrongEscrowFailed(t *testing.T) {
 	})
 }
 
+func TestWagerHandlerPayEscrowCalledOneMove(t *testing.T) {
+	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	escrow.ExpectRefund(context, alice, 45)
+	keeper.MustPayWinnings(ctx, &types.StoredGame{
+		Black:     alice,
+		Red:       bob,
+		Winner:    "b",
+		MoveCount: 1,
+		Wager:     45,
+	})
+}
+
 func TestWagerHandlerPayEscrowCalledTwoMoves(t *testing.T) {
 	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
 	ctx := sdk.UnwrapSDKContext(context)
@@ -102,6 +207,76 @@ func TestWagerHandlerPayEscrowCalledTwoMoves(t *testing.T) {
 		Red:       bob,
 		Winner:    "b",
 		MoveCount: 2,
+		Wager:     45,
+	})
+}
+
+func TestWagerHandlerRefundWrongManyMoves(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "game is not in a state to refund, move count: 2", r)
+	}()
+	keeper.MustRefundWager(ctx, &types.StoredGame{
+		MoveCount: 2,
+	})
+}
+
+func TestWagerHandlerRefundNoMoves(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	keeper.MustRefundWager(ctx, &types.StoredGame{
+		MoveCount: 0,
+	})
+}
+
+func TestWagerHandlerRefundWrongNoBlack(t *testing.T) {
+	keeper, context, ctrl, _ := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "black address is invalid: : empty address string is not allowed", r)
+	}()
+	keeper.MustRefundWager(ctx, &types.StoredGame{
+		MoveCount: 1,
+	})
+}
+
+func TestWagerHandlerRefundWrongEscrowFailed(t *testing.T) {
+	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	black, _ := sdk.AccAddressFromBech32(alice)
+	escrow.EXPECT().
+		SendCoinsFromModuleToAccount(ctx, types.ModuleName, black, gomock.Any()).
+		Times(1).
+		Return(errors.New("oops"))
+	defer func() {
+		r := recover()
+		require.NotNil(t, r, "The code did not panic")
+		require.Equal(t, "cannot refund wager to: oops", r)
+	}()
+	keeper.MustRefundWager(ctx, &types.StoredGame{
+		Black:     alice,
+		MoveCount: 1,
+		Wager:     45,
+	})
+}
+
+func TestWagerHandlerRefundCalled(t *testing.T) {
+	keeper, context, ctrl, escrow := setupKeeperForWagerHandler(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	escrow.ExpectRefund(context, alice, 45)
+	keeper.MustRefundWager(ctx, &types.StoredGame{
+		Black:     alice,
+		MoveCount: 1,
 		Wager:     45,
 	})
 }
